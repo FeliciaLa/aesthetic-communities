b4ff346c7a81f582aac308f31760f429a1811a31
FeliciaLa <feliciacarlottala@gmail.com>
2025-02-14 20:09:59 +0000

    M

--- backend/start.sh
+++ backend/start.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
+echo "Waiting for database..."
+python manage.py wait_for_db
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+echo "Running migrations..."
+python manage.py migrate --noinput
 
 echo "Starting server..."
 gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --log-level debug --timeout 30 --workers 1 --threads 2 
