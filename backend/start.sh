ea441a8c807f513160814de46c4767091ebb7e73
FeliciaLa <feliciacarlottala@gmail.com>
2025-02-14 19:54:11 +0000

    :

--- backend/start.sh
+++ backend/start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -e
 
-# Wait for database
-echo "Waiting for database..."
-python manage.py wait_for_db
-
-# Start server
-echo "Starting Gunicorn..."
+echo "Starting server..."
 gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --log-level debug --timeout 30 --workers 1 --threads 2 
